7e271d2394650a933acebf725c477a5c84546198
Shawn Miller <shawnsmm@gmail.com>
2022-02-21 18:27:00 -0500

    Initial commit

---
+++ client/build/precache-manifest.7a1565d07bd66b8444fb6cce811fe66a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "523b7d90427695a179f9873aab09bda2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "463d5e578262f4ee1fbd",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "efa753f2df22d1165778",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "463d5e578262f4ee1fbd",
+    "url": "/static/js/2.8ead090a.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.8ead090a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efa753f2df22d1165778",
+    "url": "/static/js/main.f4842bd2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
